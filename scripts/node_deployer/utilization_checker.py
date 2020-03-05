7d95f1b2ba1576b25e41b0bffd751a4f06c54900
dvolynets <d.volynets@pixelplex.io>
2020-03-05 14:34:17 +0300

    ECHO-1832: added cpu usage collection

--- scripts/node_deployer/utilization_checker.py
+++ scripts/node_deployer/utilization_checker.py
@@ -49,13 +49,14 @@ class utillization_checker:
                 process = psutil.Process(pid)
                 rssize = process.memory_info().rss
                 vmsize = process.memory_info().vms
+                cpu = process.cpu_percent(interval=1)
                 bytes = self.containers[i].exec_run(base.format(self.names[i]) + "/database").output
                 dbsize = bytes.decode('utf-8').split('\t')[0]
                 bytes = self.containers[i].exec_run(base.format(self.names[i]) + "/x86_vm").output
                 x86size = bytes.decode('utf-8').split('\t')[0]
                 bytes = self.containers[i].exec_run(base.format(self.names[i]) + "/evm").output
                 evmsize = bytes.decode('utf-8').split('\t')[0]
-                self.files[i].write("%d  %d  %s  %s  %s\n" % (rssize, vmsize, dbsize, x86size, evmsize))
+                self.files[i].write("%d  %d  %f  %s  %s  %s\n" % (rssize, vmsize, cpu, dbsize, x86size, evmsize))
                 self.files[i].flush()
             time.sleep(20)
 
