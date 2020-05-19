e8aece8f6cd026889ed227dbe6a2461e3878b733
Anton Boltach <a.boltach@pixelplex.io>
2020-05-19 18:14:59 +0300

    ECHO-2068: Refactoring start_sender

--- start_sender.py
+++ start_sender.py
@@ -1,21 +1,24 @@
+"""
+Sender transactions for Echo node
+"""
+
 #!/usr/bin/python3
 
 import argparse
-from scripts.node_sender.sender import Sender
 import time
 import traceback
 import logging
 import signal
 import sys
-import psutil
 import json
 import os
-import echopy
-import threading
-import logging
+
+import psutil
+from scripts.node_sender.sender import Sender
 
 
 def kill_sender():
+    """ Kill sender process by ID """
     my_pid = os.getpid()
     for proc in psutil.process_iter():
         if proc.name() == "start_sender.py" and proc.pid != my_pid:
@@ -23,61 +26,68 @@ def kill_sender():
             os.kill(proc.pid, signal.SIGTERM)
 
 
-sending = True
+def transfer_operation(sender, txs_count):
+    """ Send transaction with txs_count transfer operations """
+
+    return sender.transfer(transaction_count=txs_count, fee_amount=20)
 
 
-def send(sender, args, info):
-    try:
-        while sending:
-            print("Trying sent transactions to:", info, flush=True)
-            if args.tps == True:
-                start = time.time()
-                sent = send_tx(sender, args)
-                print(sent, "Transactions sent", flush=True)
-                diff = time.time() - start
-                if diff < 1.0:
-                    time.sleep(round((1.0 - diff), 3))
-            else:
-                sent = send_tx(sender, args)
-                print(sent, "Transactions sent", flush=True)
-                time.sleep(args.delay)
-    except Exception as e:
-        print("Caught exception during transaction sending", flush=True)
-        logging.error(traceback.format_exc())
+def create_evm_contract_operation(sender, txs_count):
+    """ Send transaction with txs_count create evm contract operations """
+
+    return sender.create_contract(
+        transaction_count=txs_count, x86_64_contract=False, fee_amount=395
+    )
 
 
-def send_tx(sender, args):
-    if args.tx_type == 1:
-        return sender.transfer(args.txs_count, fee_amount=20)
-    elif args.tx_type == 2:
-        return sender.create_contract(
-            transaction_count=args.txs_count, x86_64_contract=False, fee_amount=395
-        )
-    elif args.tx_type == 3:
-        return sender.create_contract(
-            transaction_count=args.txs_count, x86_64_contract=True, fee_amount=201
-        )
-    elif args.tx_type == 4:
-        return sender.call_contract(
-            contract_id="1.11.0",
-            transaction_count=args.txs_count,
-            x86_64_contract=False,
-            fee_amount=244,
-        )
-    elif args.tx_type == 5:
-        return sender.call_contract(
-            contract_id="1.11.0",
-            transaction_count=args.txs_count,
-            x86_64_contract=True,
-            fee_amount=201,
-        )
+def create_x86_contract_operation(sender, txs_count):
+    """ Send transaction with txs_count create x86 contract operations """
+
+    return sender.create_contract(
+        transaction_count=txs_count, x86_64_contract=True, fee_amount=201
+    )
 
 
-slist = []
-tlist = []
+def call_evm_contract_operation(sender, txs_count):
+    """ Send transaction with txs_count call evm contract operations """
+
+    return sender.call_contract(
+        contract_id="1.11.0",
+        transaction_count=txs_count,
+        x86_64_contract=False,
+        fee_amount=244,
+    )
 
 
-def main():
+def call_x86_contract_operation(sender, txs_count):
+    """ Send transaction with txs_count call x86 contract operations """
+
+    return sender.call_contract(
+        contract_id="1.11.0",
+        transaction_count=txs_count,
+        x86_64_contract=True,
+        fee_amount=201,
+    )
+
+
+def send_tx(sender, tx_type, txs_count):
+    """ Send transaction by operation type """
+
+    # map the inputs to the operation type
+    operation_types = {
+        1: transfer_operation,
+        2: create_evm_contract_operation,
+        3: create_x86_contract_operation,
+        4: call_evm_contract_operation,
+        5: call_x86_contract_operation,
+    }
+
+    return operation_types[tx_type](sender, txs_count)
+
+
+def parse_arguments():
+    """ Parse arguments for start sender """
+
     parser = argparse.ArgumentParser(description="Help for bm-scripts binary")
     parser.add_argument(
         "-hi",
@@ -122,7 +132,7 @@ def main():
         dest="tx_type",
         action="store",
         type=int,
-        help="Transaction type: 1 - transfer, 2 - create_evm, 3 - create_x86, 4 - call_evm, 5 - call_x86",
+        help="Transaction type: 1-transfer, 2-create_evm, 3-create_x86, 4-call_evm, 5-call_x86",
         default=1,
     )
     parser.add_argument(
@@ -152,103 +162,127 @@ def main():
         action="store_true",
         help="If specified, then sender will work concurrently",
     )
-    args = parser.parse_args()
+
+    return parser.parse_args()
+
+
+def connect_to_peers(hosts_info, number_of_accounts):
+    """ Setting up a connection to nodes """
+
+    senders = []
+    info_nodes = []
+    start_port = 8090
+    sender_id = 0
+
+    for addr, count_nodes in hosts_info.items():
+        for index in range(count_nodes):
+            try:
+                print("Trying connect to", addr, ":", start_port + index, flush=True)
+                sender = Sender(
+                    addr,
+                    start_port + index,
+                    number_of_accounts,
+                    call_id=sender_id,
+                    step=count_nodes,
+                )
+                info = "Address : {}  Port : {}".format(addr, start_port + index)
+                senders.append(sender)
+                info_nodes.append(info)
+                print("Done", flush=True)
+
+                sender_id = sender_id + 1
+            except ConnectionRefusedError as err:
+                logging.error(traceback.format_exc())
+                print(err, flush=True)
+
+    return senders, info_nodes
+
+
+def send(args, sender, info):
+    """ Send transactions with arguments """
+
+    while sender.is_interrupted is False:
+        print("Trying sent transactions to:", info, flush=True)
+        if args.tps is True:
+            start = time.time()
+            sent = send_tx(sender, args.tx_type, args.txs_count)
+            print(sent, "Transactions sent", flush=True)
+            diff = time.time() - start
+            if diff < 1.0:
+                time.sleep(round((1.0 - diff), 3))
+        else:
+            sent = send_tx(sender, args.tx_type, args.txs_count)
+            print(sent, "Transactions sent", flush=True)
+            time.sleep(args.delay)
+
+
+def run_sender(args, senders, info_nodes):
+    """ Run sender to send transactions """
+
+    while not senders:
+        for index in range(senders):
+            try:
+                send(args, senders[index], info_nodes[index])
+            except Exception as err:
+                print(
+                    "Caught exception during transaction sending: {0}".format(err),
+                    flush=True,
+                )
+                logging.error(traceback.format_exc())
+
+                del senders[index]
+                del info_nodes[index]
+
+
+def run_sender_with_subprocess():
+    """ Run some senders to send transactions with subprocess """
+
+    pass
+
+
+def main():
+    """ Main function for start sender """
+
+    args = parse_arguments()
     hosts_info = json.loads(args.hosts_info)
 
-    if args.start_new == False:
+    if args.start_new is False:
         kill_sender()
 
-    start_port = 8090
-    info_lst = []
-    prev_num_nodes = 0
-
     def signal_handler(sig, frame):
-        print("\nCaught SIGINT:")
-        sending = False
-        for s in slist:
-            s.interrupt_sender()
+        print("\nCaught signal: ", sig, ":", frame)
+        for sender in senders:
+            sender.interrupt_sender()
         raise SystemExit("Exited from Ctrl-C handler")
 
     signal.signal(signal.SIGINT, signal_handler)
 
-    step = 0
-    for addr, count in hosts_info.items():
-        step = step + count
-
-    if step > args.account_num:
+    count_nodes = sum(hosts_info.values())
+    if count_nodes > args.account_num:
         raise Exception(
             "Number of nodes should be less or equal to initial accounts number!"
         )
 
-    j = 0
-    for addr, count in hosts_info.items():
-        for i in range(count):
-            try:
-                print("Trying connect to", addr, ":", start_port + i)
-                sys.stdout.flush()
-                s = Sender(addr, start_port + i, args.account_num, call_id=j, step=step, sequence_num=args.sender_number)
-                info = "Address : {}  Port : {}".format(addr, start_port + i)
-                slist.append(s)
-                info_lst.append(info)
-                t = threading.Thread(target=send, args=(slist[j], args, info,))
-                tlist.append(t)
-                print("Done")
-                sys.stdout.flush()
-                j = j + 1
-            except ConnectionRefusedError as e:
-                logging.error(traceback.format_exc())
-                print(e)
-                sys.stdout.flush()
-        prev_num_nodes = prev_num_nodes + count
+    senders, info_nodes = connect_to_peers(hosts_info, args.account_num)
 
-    if slist:
-        if args.tx_type == 4:
-            slist[0].create_contract(x86_64_contract=False, fee_amount=400, with_response=True)
-        elif args.tx_type == 5:
-            slist[0].create_contract(x86_64_contract=True, fee_amount=201, with_response=True)
+    if not senders:
+        print("List senders is empty", flush=True)
+        sys.exit(0)
 
-        if args.parallel == False:
-            while True:
-                i = 0
-                num = len(slist)
-                if num == 0:
-                    break
-                while i < num:
-                    try:
-                        print("Trying sent transactions to:", info_lst[i], flush=True)
-                        if args.tps == True:
-                            start = time.time()
-                            sent = send_tx(slist[i], args)
-                            print(sent, "Transactions sent", flush=True)
-                            diff = time.time() - start
-                            if diff < 1.0:
-                                time.sleep(round((1.0 - diff), 3))
-                        else:
-                            sent = send_tx(slist[i], args)
-                            print(sent, "Transactions sent")
-                            sys.stdout.flush()
-                            time.sleep(args.delay)
-                        i = i + 1
-                    except Exception as e:
-                        print("Caught exception during transaction sending")
-                        sys.stdout.flush()
-                        logging.error(traceback.format_exc())
-                        sys.stdout.flush()
-                        del slist[i]
-                        del info_lst[i]
-                        num = num - 1
-        else:
-            for t in tlist:
-                t.daemon = True
-                t.start()
-            for t in tlist:
-                t.join()
+    if args.tx_type == 4 or args.tx_type == 5:
+        send_tx(senders[0], args.tx_type, 1)
+
+    if args.parallel is False:
+        run_sender(args, senders, info_nodes)
+    else:
+        run_sender_with_subprocess()
 
 
 if __name__ == "__main__":
     try:
         main()
-    except SystemExit as e:
-        print(e)
-    except Exception as e:
+    except SystemExit as err:
+        print(err)
+    except Exception as err:
+        print(err)
         logging.error(traceback.format_exc())
